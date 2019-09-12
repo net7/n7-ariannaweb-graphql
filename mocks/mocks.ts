e2abc0d1c6cf6fcaeb023f53c599e93041d98ba2
dariopiotN7 <piotrowicz@netseven.it>
2019-09-12 12:38:10 +0200

    silly hero mock test with input for initial-setup

--- mocks/mocks.ts
+++ mocks/mocks.ts
@@ -6,6 +6,8 @@ import { Helpers } from './helpers';
 // Inserire qui i mock per le varie query
 export const mocks = {
   Query:() => ({
+    getTestHero: (_,args) => { if(args.title) return { title:args.title };
+                                else return {title: "default title"}; }
   }),
   Mutation: () => ({
   })
