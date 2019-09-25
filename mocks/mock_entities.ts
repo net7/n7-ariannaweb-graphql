c8106e486eeeaaa7f9cd9d8cf5e3cf10130073bd
dariopiotN7 <piotrowicz@netseven.it>
2019-09-25 13:28:45 +0200

    entity details mock improved + bug-fix

--- mocks/mock_entities.ts
+++ mocks/mock_entities.ts
@@ -82,13 +82,56 @@ export function getEntityDetails(entityId:String){
 
   return {
     entity: basicEntity,
-    overviewTab: "Milano e' un comune italiano di 1372810 abitanti," +
-                 " capoluogo della regione Lombardia, dell'omonima citta' matropolitana," +
-                 " e centro di una delle piu' popolose aree metropolitane d'Europa. Etc...",
+    overviewTab: Helpers.randomPick(allPossibleOverviewTabs),
     fieldsTab: makeRandomFieldGroups(),
-    Items: generateRandomBunchOfItemListings(allPossibleThumbnails),
+    items: generateRandomBunchOfItemListings(allPossibleThumbnails),
     entities,
-    extraTabUrl: null,
-    wikiTabUrl: null
+    extraTabUrl: Helpers.randomPick(allPossibleExtraUrls),
+    wikiTabUrl: Helpers.randomPick(allPossibleWikiUrls)
   }
 }
+
+const allPossibleExtraUrls = [
+  'http://example.com/',
+  'http://www.example.com/',
+  'http://bead.example.com/',
+  'https://agreement.example.com/',
+  'http://www.example.edu/border.php#afterthought',
+  'https://www.example.com/board?action=bone',
+  null
+];
+
+
+const allPossibleWikiUrls = [
+  'https://it.wikipedia.org/wiki/Lorem_ipsum',
+  'https://en.wikipedia.org/wiki/Alighiero_Boetti',
+  'https://it.wikipedia.org/wiki/Milano',
+  'https://it.wikipedia.org/wiki/Concetto',
+  null
+];
+
+
+const allPossibleOverviewTabs = [
+ `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+  ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+  laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+  voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+  non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+  `Milano e' un comune italiano di 1372810 abitanti, capoluogo della regione Lombardia,
+  dell'omonima citta' matropolitana, e centro di una delle piu' popolose aree metropolitane
+  d'Europa. Etc...`,
+  `Alighiero Boetti (1940-1994) – o Alighiero e Boetti come si firma a partire dal 1971 –
+  nasce a Torino dove esordisce nell’ambito dell’Arte Povera nel gennaio del 1967.
+  Nel 1972 si trasferisce a Roma, contesto più affine alla sua predilezione per il Sud del mondo.
+  Già l’anno precedente ha scoperto l’Afghanistan e avviato il lavoro artistico che affida alle
+  ricamatrici afghane, tra cui le Mappe, i planisferi colorati che riproporrà lungo gli anni,
+  come registro dei mutamenti politici del mondo.`,
+  `Il concetto (o nozione, intesa come cognizione fondamentale) in senso lato è un'idea astratta
+  e generale che viene espressa in maniera definita con un procedimento che raccoglie e aggrega
+  ("concetto" dal latino concipĕre = cum-capĕre, comprehendĕre) aspetti sensibili particolari che
+  una molteplicità di oggetti hanno in comune. Con il concetto la mente è in grado di avere
+  chiari i caratteri essenziali e costanti di una specifica realtà. Questi aspetti sostanziali,
+  chiamati nella Logica "note definitorie", con il concetto saranno presenti alla mente che sarà
+  in grado di riconoscere, senza dover procedere a ulteriori elaborazioni, tutti quegli oggetti
+  che presentano il complesso di quelle stesse caratteristiche particolari.`
+];
