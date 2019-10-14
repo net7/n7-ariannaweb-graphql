fd9ddbf181936c50ae7ec59c70b069828bd38f65
nino <antonino.dalberti@gmail.com>
2019-10-14 17:04:15 +0200

    aggiunta funzione getEntitiesCountData

--- datasources/datasources.ts
+++ datasources/datasources.ts
@@ -1,4 +1,5 @@
-import {search} from "./elasticsearch"
+import { search } from "./elasticsearch"
+import { json } from "body-parser"
 
 export async function getEntity(entityId: string) {
 	if (entityId == null || entityId === '')
@@ -13,6 +14,41 @@ export async function getEntity(entityId: string) {
 			}
 		}
 	}
-	const res = await search(query)
-	return res.result[0];
+	const body = await search(query)
+	const res = body['hit']['hit'].map(x => x['_source'])
+	return res;
+}
+
+export async function getEntitiesCountData() {
+	var query = {
+		index: 'cultural_objects',
+		body: {
+			"aggs": {
+				"entities": {
+					"nested": {
+						"path": "connectedEntities"
+					},
+					"aggs": {
+						"doc_per_entities": {
+							"terms": {
+								"min_doc_count": 1,
+								"script": " '{\"id\":\"' + doc['connectedEntities.id'].value + '\", \"label\":\"' + doc['connectedEntities.label'].value + '\", \"typeOfEntity\":{\"configKey\":\"' +  doc['connectedEntities.typeOfEntity.configKey'].value + '\", \"label\":\"' + doc['connectedEntities.typeOfEntity.label'].value + '\", \"id\":\"' + doc['connectedEntities.typeOfEntity.id'].value + '\"}}'",
+								"size": 10000
+							}
+						}
+					}
+				}
+			},
+			"size": 0
+		}
+	}
+	const body = await search(query)
+	const res = body.aggregations.entities.doc_per_entities.buckets.map((x: any) => {
+		x.entity = JSON.parse(x.key)
+		x.count = x.doc_count
+		delete x.key
+		delete x.doc_count
+		return x
+	})
+	return res;
 }
--- resolvers/mainResolvers.ts
+++ resolvers/mainResolvers.ts
@@ -6,6 +6,7 @@ const externalResolvers = [entity].map(x => x.resolvers)
 
 export const resolvers = merge({
 	Query: {
+		entitiesCountData: async () => await datasources.getEntitiesCountData(), 
 		getEntity: async (parent, args, context, info) => await datasources.getEntity(args.entityId)
 	},
 }, ...externalResolvers)
