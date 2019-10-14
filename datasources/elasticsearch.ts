f439aadb1a1ae75d1622c68a6ee5a06fdd4dc1d7
nino <antonino.dalberti@gmail.com>
2019-10-14 17:02:23 +0200

    risposta elasticsearch più generica

--- datasources/elasticsearch.ts
+++ datasources/elasticsearch.ts
@@ -5,9 +5,5 @@ const client = new Client({ node: 'https://95.216.97.241:9200', auth: auth, ssl:
 
 export async function search(query) {
 	const { body } = await client.search(query)
-	const res = {
-		total: body['hits']['total'],
-		result: body['hits']['hits'].map(x => x['_source'])
-	}
-	return res;
+	return body;
 }
