3ed44cab56261632f30b447e1f798281f4c34946
nino <antonino.dalberti@gmail.com>
2019-10-14 09:52:23 +0200

    rimosse funzioni di chiamate a elasticsearch

--- resolvers/entity.ts
+++ resolvers/entity.ts
@@ -1,23 +1,5 @@
-import {search} from "./elasticsearch"
 
 export const resolvers = {
 	Entity: {
 	}
-}
-
-export async function getEntity(entityId: string) {
-	if (entityId == null || entityId === '')
-		return null
-	var query = {
-		index: 'entities',
-		body: {
-			query: {
-				match: {
-					id: entityId
-				}
-			}
-		}
-	}
-	const res = await search(query)
-	return res.result[0];
 }
