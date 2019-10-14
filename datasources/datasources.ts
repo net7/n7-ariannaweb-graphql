7440300d42672cc086c20d686ea052509c9c862f
nino <antonino.dalberti@gmail.com>
2019-10-14 09:53:41 +0200

    aggiunto file di chiamate elasticsearch

---
+++ datasources/datasources.ts
@@ -0,0 +1,18 @@
+import {search} from "./elasticsearch"
+
+export async function getEntity(entityId: string) {
+	if (entityId == null || entityId === '')
+		return null
+	var query = {
+		index: 'entities',
+		body: {
+			query: {
+				match: {
+					id: entityId
+				}
+			}
+		}
+	}
+	const res = await search(query)
+	return res.result[0];
+}
