8374ecfa956666d45fa3fc0acbde9663216ce7f1
nino <antonino.dalberti@gmail.com>
2019-10-16 12:19:10 +0200

    aggiunto il resolver della globalFIlter

--- resolvers/mainResolvers.ts
+++ resolvers/mainResolvers.ts
@@ -6,8 +6,7 @@ const externalResolvers = [entity].map(x => x.resolvers)
 
 export const resolvers = merge({
 	Query: {
-		globalFilter: async (parent, args, context, info) => await datasources.getEntitiesCountData(args.selectedEntitiesIds),
-		entitiesCountData: async () => await datasources.getEntitiesCountData(), 
+		globalFilter: async (parent, args, context, info) => await datasources.getItemsFiltered(args.typeOfEntityFilter, args.selectedEntitiesIds, args.itemsPagination),
 		getEntity: async (parent, args, context, info) => await datasources.getEntity(args.entityId)
 	},
 }, ...externalResolvers)
