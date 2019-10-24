a55ca26be6630e82ae599ecff49128fc840d9f01
nino <antonino.dalberti@gmail.com>
2019-10-24 09:40:01 +0200

    spostamento delle funziomi per costruire le query nel file di elasticsearch

--- datasources/datasources.ts
+++ datasources/datasources.ts
@@ -1,137 +1,4 @@
-import { search } from "./elasticsearch"
-
-/**
- * 
- * @param index index name
- * @param body http body for a query request
- */
-const queryBuilder = (index: string, body: any) => {
-	const x = {
-		index: index,
-		body: body
-	}
-	return x
-}
-
-/*get the path and a generic query and return a nested query block*/
-/**
- * 
- * @param path pathname to reace the nested field
- * @param query query to apply on a nested object field
- */
-const queryNested = (path: string, query: any) => {
-	const x = {
-		query: {
-			nested: {
-				path: path,
-				query: query.query
-			}
-		}
-	}
-	return x
-}
-
-/**
- * 
- * @param field field name
- * @param script script to generate a script field
- */
-const scriptFields = (field: string, script: string) => {
-	const x = {
-		script_fields: {}
-	}
-	x.script_fields[field] = {
-		script: {
-			source: script
-		}
-	}
-	return x
-}
-
-/**
- * 
- * @param buckets buckets name
- * @param path path of a nested field
- * @param aggs aggs block to apply on a nested object field
- */
-const aggsNested = (buckets: string, path: string, aggs: any) => {
-	const x = {
-		aggs: {}
-	}
-	x.aggs[buckets] = {
-		nested: {
-			path: path,
-		},
-		aggs: aggs.aggs
-	}
-	return x
-}
-
-/**
- * 
- * @param buckets buckets name
- * @param field field to aggregate
- * @param script script to aggregate field in a custom way 
- * @param size max number of buckets returned
- */
-const aggsTerms = (buckets: string, field: string = null, script: string = null, size: number = 10000) => {
-	const x = {
-		aggs: {}
-	}
-	x.aggs[buckets] = {
-		terms: {
-			min_doc_count: 1,
-			size: size,
-		}
-	}
-
-	if (field != null)
-		x.aggs[buckets].terms['field'] = field
-	if (script != null)
-		x.aggs[buckets].terms['script'] = script
-	return x
-}
-
-/**
- * 
- * @param mustList list of query blocks to insert in multi-conditions block
- */
-const queryBool = (mustList: any[]) => {
-	const x = {
-		query: {
-			bool: {
-				must: mustList
-			}
-		}
-	}
-	return x
-}
-
-/**
- * 
- * @param queryField object containing the field name and the value to search on it
- */
-const queryString = (queryField: { field: string, value: string }) => {
-	const x = {
-		query_string: {
-			query: "*" + queryField.value.trim() + "*",
-			default_field: queryField.field
-		}
-	}
-	return x
-}
-
-/**
- * 
- * @param termField object containing the field name as key and the field value to search as the value
- */
-const queryTerm = (termField: any) => {
-	return {
-		query: {
-			term: termField
-		}
-	}
-}
+import * as el from "./elasticsearch"
 
 /**
  * 
@@ -141,8 +8,8 @@ export async function getEntity(entityId: string) {
 	if (entityId == null || entityId === '')
 		return null
 
-	const query = queryBuilder('entities', queryTerm({ id: entityId }))
-	const body = await search(query)
+	const query = el.queryBuilder('entities', el.queryTerm({ id: entityId }))
+	const body = await el.search(query)
 	const res = body.hits.hits
 	if (res.length > 0)
 		return res[0]._source;
@@ -155,8 +22,8 @@ export async function getEntity(entityId: string) {
  * @param itemId item Id to recall corresponding item
  */
 export async function getItem(itemId: string) {
-	const query = queryBuilder('cultural_objects', queryTerm({ id: itemId }))
-	const body = await search(query)
+	const query = el.queryBuilder('cultural_objects', el.queryTerm({ id: itemId }))
+	const body = await el.search(query)
 	const res = body.hits.hits
 	if (res.length > 0)
 		return res[0]._source;
@@ -172,23 +39,23 @@ export async function getItem(itemId: string) {
  */
 export async function getEntitiesFiltered(input: string, itemsPagination: { limit: number, offset: number } = { limit: 100000, offset: 0 }, typeOfConfigKey: string) {
 
-	const q1 = queryTerm({ "typeOfEntity.configKey": typeOfConfigKey })
-	const q2 = queryString({ field: 'label', value: input })
-	const bools = queryBool([q1.query, q2])
-	const query = queryBuilder('entities', {
+	const q1 = el.queryTerm({ "typeOfEntity.configKey": typeOfConfigKey })
+	const q2 = el.queryString({ field: 'label', value: input })
+	const bools = el.queryBool([q1.query, q2])
+	const query = el.queryBuilder('entities', {
 		query: bools.query,
 		size: itemsPagination.limit,
 		from: itemsPagination.offset
 	})
 
-	const q3 = queryTerm({ "connectedEntities.typeOfEntity.configKey": typeOfConfigKey })
-	const q4 = queryString({ field: 'connectedEntities.label', value: input })
-	const bools2 = queryBool([q3.query, q4])
-	const quNes = queryNested('connectedEntities', bools2)
-	const agg = aggsTerms('docsPerEntity', "connectedEntities.id")
-	const agNes = aggsNested('entities', 'connectedEntities', agg)
+	const q3 = el.queryTerm({ "connectedEntities.typeOfEntity.configKey": typeOfConfigKey })
+	const q4 = el.queryString({ field: 'connectedEntities.label', value: input })
+	const bools2 = el.queryBool([q3.query, q4])
+	const quNes = el.queryNested('connectedEntities', bools2)
+	const agg = el.aggsTerms('docsPerEntity', "connectedEntities.id")
+	const agNes = el.aggsNested('entities', 'connectedEntities', agg)
 
-	const query2 = queryBuilder('cultural_objects', {
+	const query2 = el.queryBuilder('cultural_objects', {
 		query: quNes.query,
 		aggs: agNes.aggs,
 		size: 0
@@ -197,7 +64,7 @@ export async function getEntitiesFiltered(input: string, itemsPagination: { limi
 	const entityHashMap = {}
 
 	const res = await Promise.all(
-		[search(query), search(query2).then(res => res.aggregations.
+		[el.search(query), el.search(query2).then(res => res.aggregations.
 			entities.docsPerEntity.buckets.forEach(x => {
 				entityHashMap[x.key] = x.doc_count
 			}))])
@@ -220,10 +87,10 @@ export async function getEntitiesFiltered(input: string, itemsPagination: { limi
 export async function getItemsFiltered(entityIds: [string], itemsPagination: { limit: number, offset: number } = { limit: 100000, offset: 0 }, entitiesListSize: number = 10000) {
 
 	const script = "'{\"id\":\"' + doc['connectedEntities.id'].value + '\",\"label\":\"' + doc['connectedEntities.label'].value + '\", \"typeOfEntity\":{\"configKey\":\"' + doc['connectedEntities.typeOfEntity.configKey'].value + '\", \"label\":\"' + doc['connectedEntities.typeOfEntity.label'].value + '\", \"id\":\"' + doc['connectedEntities.typeOfEntity.id'].value + '\"}}'"
-	const agg = aggsTerms("docsPerEntity", null, script, entitiesListSize)
-	const agNes = aggsNested('entities', 'connectedEntities', agg)
+	const agg = el.aggsTerms("docsPerEntity", null, script, entitiesListSize)
+	const agNes = el.aggsNested('entities', 'connectedEntities', agg)
 	const source = "def list = new HashMap(); for (type in params['_source'].connectedEntities) { def key = type.typeOfEntity.configKey; if(list[key] != null){list[key]['count']++;} else { list[key] = new HashMap(); list[key]['count'] = 1; list[key]['type'] = new HashMap(); list[key]['type']['configKey'] = type.typeOfEntity.configKey; list[key]['type']['id'] = type.typeOfEntity.id; list[key]['type']['label'] = type.typeOfEntity.label}} return list;"
-	const scFi = scriptFields('typeOfEntitiesCount', source)
+	const scFi = el.scriptFields('typeOfEntitiesCount', source)
 
 	const body = {
 		aggs: agNes.aggs, script_fields: scFi.script_fields,
@@ -235,14 +102,14 @@ export async function getItemsFiltered(entityIds: [string], itemsPagination: { l
 	const entities = []
 	if (entityIds != null && entityIds.length > 0) {
 		for (const entityId of entityIds) {
-			entities.push(queryNested("connectedEntities", queryTerm({ "connectedEntities.id": entityId })).query)
+			entities.push(el.queryNested("connectedEntities", el.queryTerm({ "connectedEntities.id": entityId })).query)
 		}
-		body['query'] = queryBool(entities)
+		body['query'] = el.queryBool(entities)
 	}
 
-	const query = queryBuilder('cultural_objects', body)
+	const query = el.queryBuilder('cultural_objects', body)
 
-	const res = await search(query)
+	const res = await el.search(query)
 	const buckets = res.aggregations.entities.docsPerEntity.buckets
 
 	const results = await Promise.all([
--- datasources/elasticsearch.ts
+++ datasources/elasticsearch.ts
@@ -1,9 +1,146 @@
 import { Client } from '@elastic/elasticsearch'
-import { elasticAuth as auth } from "./credentials"
+import { elasticAuth as auth, elasticNodeAddress as addr } from "./elasticConfig"
 
-const client = new Client({ node: 'https://95.216.97.241:9200', auth: auth, ssl: { rejectUnauthorized: false } })
+const client = new Client({ node: addr, auth: auth, ssl: { rejectUnauthorized: false } })
 
-export async function search(query) {
-	const { body } = await client.search(query)
-	return body;
+/**
+ * 
+ * @param body http body request for elasticsearch query
+ */
+export async function search(body: any) {
+	const { body: res } = await client.search(body)
+	return res;
+}
+
+/**
+ * 
+ * @param index index name
+ * @param body http body for a query request
+ */
+export const queryBuilder = (index: string, body: any) => {
+	const x = {
+		index: index,
+		body: body
+	}
+	return x
+}
+
+/*get the path and a generic query and return a nested query block*/
+/**
+ * 
+ * @param path path to reace the nested field
+ * @param query query to apply on a nested object field
+ */
+export const queryNested = (path: string, query: any) => {
+	const x = {
+		query: {
+			nested: {
+				path: path,
+				query: query.query
+			}
+		}
+	}
+	return x
+}
+
+/**
+ * 
+ * @param field field name
+ * @param script script to generate a script field
+ */
+export const scriptFields = (field: string, script: string) => {
+	const x = {
+		script_fields: {}
+	}
+	x.script_fields[field] = {
+		script: {
+			source: script
+		}
+	}
+	return x
+}
+
+/**
+ * 
+ * @param buckets buckets name
+ * @param path path of a nested field
+ * @param aggs aggs block to apply on a nested object field
+ */
+export const aggsNested = (buckets: string, path: string, aggs: any) => {
+	const x = {
+		aggs: {}
+	}
+	x.aggs[buckets] = {
+		nested: {
+			path: path,
+		},
+		aggs: aggs.aggs
+	}
+	return x
+}
+
+/**
+ * 
+ * @param buckets buckets name
+ * @param field field to aggregate
+ * @param script script to aggregate field in a custom way 
+ * @param size max number of buckets returned
+ */
+export const aggsTerms = (buckets: string, field: string = null, script: string = null, size: number = 10000) => {
+	const x = {
+		aggs: {}
+	}
+	x.aggs[buckets] = {
+		terms: {
+			min_doc_count: 1,
+			size: size,
+		}
+	}
+
+	if (field != null)
+		x.aggs[buckets].terms['field'] = field
+	if (script != null)
+		x.aggs[buckets].terms['script'] = script
+	return x
+}
+
+/**
+ * 
+ * @param mustList list of query blocks to insert in multi-conditions block
+ */
+export const queryBool = (mustList: any[]) => {
+	const x = {
+		query: {
+			bool: {
+				must: mustList
+			}
+		}
+	}
+	return x
+}
+
+/**
+ * 
+ * @param queryField object containing the field name and the value to search on it
+ */
+export const queryString = (queryField: { field: string, value: string }) => {
+	const x = {
+		query_string: {
+			query: "*" + queryField.value.trim() + "*",
+			default_field: queryField.field
+		}
+	}
+	return x
+}
+
+/**
+ * 
+ * @param termField object containing the field name as key and the field value to search as the value
+ */
+export const queryTerm = (termField: any) => {
+	return {
+		query: {
+			term: termField
+		}
+	}
 }
