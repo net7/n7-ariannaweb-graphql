35cd22adba926e65877de96640e063adf9398b80
nino <antonino.dalberti@gmail.com>
2019-11-06 10:17:30 +0100

    aggiunto search e il resolver della classe Field

--- resolvers/mainResolvers.ts
+++ resolvers/mainResolvers.ts
@@ -1,14 +1,15 @@
 import { merge } from 'lodash'
 import * as datasources from '../datasources/datasources'
-import * as entity from './entity'
+import * as field from './Field'
 
-const externalResolvers = [entity].map(x => x.resolvers)
+const externalResolvers = [field].map(x => x.resolvers)
 
 export const resolvers = merge({
 	Query: {
 		getItem: async (parent, args, context, info) => await datasources.getItem(args.itemId, args.maxSimilarItems, args.entitiesListSize),
 		autoComplete: async (parent, args, context, info) => await datasources.getEntitiesFiltered(args.input, args.itemsPagination, args.typeOfEntity),
 		globalFilter: async (parent, args, context, info) => await datasources.getItemsFiltered(args.selectedEntitiesIds, args.itemsPagination, args.entitiesListSize),
-		getEntity: async (parent, args, context, info) => await datasources.getEntity(args.entityId, args.itemsPagination, args.entitiesListSize)
+		getEntity: async (parent, args, context, info) => await datasources.getEntity(args.entityId, args.itemsPagination, args.entitiesListSize),
+		search: async (parent, args, context, info) => await datasources.search(args.searchParameters)
 	},
 }, ...externalResolvers)
