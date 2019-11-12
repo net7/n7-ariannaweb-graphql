f12743229e4a7d06209e8626981f6a77bbfc3997
nino <antonino.dalberti@gmail.com>
2019-11-12 11:34:32 +0100

    rimozione mock import

--- index.ts
+++ index.ts
@@ -6,12 +6,9 @@ var path = require ( 'path' );
 
 const typeDefs = importSchema(path.join(__dirname, "schema.graphql"));
 
-import { mocks } from './mocks/mocks';
-
 const server = new ApolloServer({
   typeDefs: typeDefs,
   resolvers,
-  //mockEntireSchema: false,
   mocks: false,
   playground: true, // playgound to work on zeit.co should anyway be removed before production
 });
